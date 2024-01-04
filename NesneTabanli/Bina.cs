f31c5a52ba3bd0651c78fcd64cbb0af0386d118b
Alihan Uslu <alihansmac@Alihan-MBP.home>
2024-01-04 23:21:07 +0300

    last updates

---
+++ NesneTabanli/Bina.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace NesneTabanli
+{
+	public class Bina
+	{
+		public int binano;
+		public string binaadi;
+		public string adres;
+		public int sum;
+
+		public void Yazdir1()
+		{
+			Console.WriteLine("");
+			Console.WriteLine("zil çalınıyor");
+
+
+		}
+
+
+
+
+
+		public void Yazdir()
+		{
+			Console.WriteLine(" ");
+			Console.WriteLine("binamıza hoşgeldiniz");
+			
+		}
+
+
+
+
+        public Bina(int binano, string binaadi, string adres)
+		{
+			this.binano = binano;
+			this.binaadi = binaadi;
+			this.adres = adres;
+
+		}
+
+		public Bina()
+		{
+
+		}
+
+		public enum daireler
+		{
+			daire = 1,
+			daire2 = 2,
+			daire3 = 3,
+			daire4 = 4,
+			daire5 = 5,
+			daire6 = 6
+		}
+
+    }
+	
+	
+}
+
--- NesneTabanli/Program.cs
+++ NesneTabanli/Program.cs
@@ -105,7 +105,7 @@ ali.Yazdir();
 
 
 
-
+/*
 using NesneTabanli;
 
 
@@ -165,8 +165,152 @@ else
     Console.WriteLine("lütfen geçerli bir marka giriniz");
 }
 
+*/
+
+using NesneTabanli;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+
+
+            Bina genel = new Bina();
+
+
+            Console.WriteLine("kapıya hoşgeldiniz öncelikle lütfen nerde olduğunuzu görmek için 1 e basınız");
+            Console.WriteLine(" ");
+
+            genel.sum = Convert.ToInt32(Console.ReadLine());
+
+
+            if (genel.sum == 1)
+            {
+                genel.adres = "istanbul/Levent";
+                genel.binaadi = "beyza";
+                genel.binano = 38;
+
+                Console.WriteLine("adres: " + genel.adres + "  bina adı: " + genel.binaadi + "  bina no: " + genel.binano);
+                genel.Yazdir();
+                Console.WriteLine(" ");
+            }
+            else
+            {
+                Console.WriteLine("yanlış bina veya adres");
+                Console.WriteLine(" ");
+                Console.WriteLine("iyi günler dileriz");
+            }
+
+            Bina.daireler sec = new Bina.daireler();
+            
+
+            Console.WriteLine("lütfen zilini çalmak ve bilgilerini görmek istediğiniz daireyi seçiniz");
+            Console.WriteLine("");
+
+            Console.WriteLine("1:Alihan uslu");
+            Console.WriteLine("");
+
+            Console.WriteLine("2:Ahmet tandoğan");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("3:Ömer faruk sanal");
+            Console.WriteLine("");
+
+            Console.WriteLine("4:Mustafa akdal");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("5:Leyla pamuk");
+            Console.WriteLine("");
+
+            Console.WriteLine("6:Deniz okumuş");
+            Console.WriteLine("");
+
+            sec = (Bina.daireler)(Convert.ToInt32(Console.ReadLine()));
+
+            switch (sec)
+            {
+
+                case Bina.daireler.daire:
+                    Bina alihan = new Bina();
+
+                    alihan.adres = "birinci kat sağ taraf ";
+                    Console.WriteLine(" ");
+                    Console.WriteLine(alihan.adres);
+                    Console.WriteLine(" ");
+                    alihan.Yazdir1();
+                    break;
+
+
+                case Bina.daireler.daire2:
+                    Bina ahmet = new Bina();
+
+                    ahmet.adres = "birinci kat sol taraf ";
+                    Console.WriteLine(" ");
+                    Console.WriteLine(ahmet.adres);
+                    Console.WriteLine(" ");
+                    ahmet.Yazdir1();
+                   
+                    break;
+
+
+                case Bina.daireler.daire3:
+                    Bina omer = new Bina();
+
+                    omer.adres = "ikinci kat sağ taraf ";
+                    Console.WriteLine(" ");
+                    Console.WriteLine(omer.adres);
+                    Console.WriteLine(" ");
+                    omer.Yazdir1();
+                    break;
+
+
+                case Bina.daireler.daire4:
+                    Bina mustafa = new Bina();
+
+                    mustafa.adres = "ikinci kat sol taraf ";
+                    Console.WriteLine(" ");
+                    Console.WriteLine(mustafa.adres);
+                    Console.WriteLine(" ");
+                    mustafa.Yazdir1();
+                    break;
+
+
+                case Bina.daireler.daire5:
+                    Bina leyla = new Bina();
+
+                    leyla.adres = "üçüncü kat sağ taraf ";
+                    Console.WriteLine(" ");
+                    Console.WriteLine(leyla.adres);
+                    Console.WriteLine(" ");
+                    leyla.Yazdir1();
+                    break;
+
+
+                case Bina.daireler.daire6:
+                    Bina deniz = new Bina();
+
+                    deniz.adres = " üçüncü kat sol taraf ";
+                    Console.WriteLine(" ");
+                    Console.WriteLine(deniz.adres);
+                    Console.WriteLine(" ");
+                    deniz.Yazdir1();
+                    break;
+
+
+                default:
+                    break;
+
+            }
+
+       
 
 
 
 
 
+
+        }
+    }
+}
