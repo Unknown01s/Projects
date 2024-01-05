c338a5e2e98701e3128aa0896272835c54b49529
Alihan Uslu <alihansmac@Alihan-MBP.home>
2024-01-05 16:14:30 +0300

    last updates

---
+++ testnesne/src/Bina.java
@@ -0,0 +1,32 @@
+public class Bina {
+    public int binano;
+    public String binaadi;
+    public String adres;
+
+
+    public Bina(int binano,String binaadi,String adres){
+
+        this.binano = binano;
+        this.adres = adres;
+        this.binaadi = binaadi;
+
+    }
+
+
+    public void Write(){
+
+        System.out.println("zil çalınıyor");
+    }
+
+    public void Write1(){
+
+        System.out.println("Yanlış bina ya da adres iyi günler dileriz");
+    }
+
+
+    public Bina(){
+
+    }
+
+
+}
--- testnesne/src/Main.java
+++ testnesne/src/Main.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public enum daireler{
+        daire1,
+        daire2,
+        daire3,
+        daire4,
+        daire5,
+        daire6
+    }
+
+
     public static void main(String[] args) {
 
 /*
@@ -25,7 +35,7 @@ public class Main {
         System.out.println(araba.year);
         araba.Yazdir();
 
-*/
+
 
         Car araba = new Car();
         Scanner scanner =  new Scanner(System.in);
@@ -49,6 +59,101 @@ public class Main {
         System.out.println(" ");
 
         araba.Yazdir();
+
+
+    }
+}
+
+ */
+
+
+        System.out.println("Binamıza Hoş geldiniz Lütfen Bilgileri görmek için 1 e basınız");
+        Scanner scanner = new Scanner(System.in);
+
+        int res = scanner.nextInt();
+
+        Bina sec = new Bina();
+
+        sec.adres = "Istanbul";
+        sec.binano = 38;
+        sec.binaadi = "Beyza Apartmanı";
+
+
+        if (res == 1) {
+            System.out.println("adres " + sec.adres + " no " + sec.binano + " adı " + sec.binaadi);
+        } else {
+            sec.Write1();
+        }
+
+        System.out.println("");
+        System.out.println("Lütfen Zilini çalmak istediğiniz ve Bilgileri görmek istediğiniz daireyi seçin");
+        System.out.println("");
+
+
+        System.out.println("1.Alihan Uslu");
+        System.out.println("");
+
+        System.out.println("2.Ragıp Aşkar");
+        System.out.println("");
+
+        System.out.println("3.Guşan Saygın");
+        System.out.println("");
+
+        System.out.println("4.MISTIK");
+        System.out.println("");
+
+        System.out.println("5.Ömer Aytemiz");
+        System.out.println("");
+
+        System.out.println("6.Gökberk Mahomes");
+        System.out.println("");
+
+
+
+        daireler daire = daireler.values()[scanner.nextInt()];
+
+        switch (daire) {
+            case daire1:
+                Bina alihan = new Bina(5,"1.kat","asansörün sağ tarafı");
+                System.out.println(" ");
+                System.out.println(alihan.binano + alihan.binaadi + alihan.adres);
+                alihan.Write();
+                break;
+            case daire2:
+                Bina ragıp = new Bina(6,"1.kat","asansörün sol tarafı");
+                System.out.println(" ");
+                System.out.println(ragıp.binano + ragıp.binaadi + ragıp.adres);
+                ragıp.Write();
+                break;
+            case daire3:
+                Bina gusan = new Bina(7,"2.kat","asansörün sağ tarafı");
+                System.out.println(" ");
+                System.out.println(gusan.binano + gusan.binaadi + gusan.adres);
+                gusan.Write();
+                break;
+            case daire4:
+                Bina mustafa = new Bina(8,"2.kat","asansörün sol tarafı");
+                System.out.println(" ");
+                System.out.println(mustafa.binano + mustafa.binaadi + mustafa.adres);
+                mustafa.Write();
+                break;
+            case daire5:
+                Bina omer = new Bina(9,"3.kat","asansörün sağ tarafı");
+                System.out.println(" ");
+                System.out.println(omer.binano + omer.binaadi + omer.adres);
+                omer.Write();
+                break;
+            case daire6:
+                Bina gokberk = new Bina(10,"3.kat","asansörün sol tarafı");
+                System.out.println(gokberk.binano  + " " + gokberk.binaadi + " "  + gokberk.adres);
+                System.out.println(" ");
+                gokberk.Write();
+                break;
+        }
+
+
+    }
+    {
 
 
     }
